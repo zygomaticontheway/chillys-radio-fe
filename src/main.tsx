1807c1c74cedd9af52ac9822c5e0a899315f985e
zygomaticontheway <mrzygomatic@gmail.com>
2024-09-25 12:08:25 +0200

    deleted comments with Layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./components/homepage/Homepage";
-// import { Layout } from "./components/layout/Layout";
 import RickAstley from "./components/rickAstley/RickAstley";
 import { store } from "./redux/store";
+import React from "react";
 
 
 createRoot(document.getElementById('root')!).render(
@@ -12,10 +12,8 @@ createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <HashRouter>
       <Routes>
-        {/* <Route path='/' element={<Layout />} > */}
           <Route path='/' element={<Homepage />} />
           <Route path='*' element={<div className='lesson-container'><h1>ERROR 404 ☠️</h1> <RickAstley /></div>} />
-        {/* </Route> */}
       </Routes>
     </HashRouter>
   </Provider>
