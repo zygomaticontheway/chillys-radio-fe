e69acdcb639fdaf7269b2e5fa8b210df6b20195e
zygomaticontheway <mrzygomatic@gmail.com>
2024-09-30 12:00:29 +0200

    footer.module.css fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './Footer.module.css';
+import styles from './footer.module.css';
 import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
