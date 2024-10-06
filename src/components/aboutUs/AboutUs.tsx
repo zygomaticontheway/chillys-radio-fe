31657279c5014692d0fba5771e98399f6ff264df
Liubov Korchashkina <korchashkina.liubov@gmail.com>
2024-10-06 17:45:00 +0200

    add about us main logic

--- src/components/aboutUs/AboutUs.tsx
+++ src/components/aboutUs/AboutUs.tsx
@@ -4,16 +4,16 @@ import styles from './aboutUs.module.css';
 const developers = [
   { name: "Oleksii Ilchenko", specialization: "Fullstack Developer, teamlead", git: "https://github.com/zygomaticontheway/", linkedin: "https://www.linkedin.com/in/aleksey-ilchenko/" },
   { name: "Valerii Kuchynskyi", specialization: "Fullstack Developer", git: "", linkedin: "" },
-  { name: "Liubov Korchashkina", specialization: "Fullstack Developer", git: "https://github.com/svetlana-svetlova", linkedin: "https://www.linkedin.com/in/svetlana-svetlova" },
-  { name: "Алексей Алексеев", specialization: "Fullstack Developer", git: "https://github.com/aleksey-alekseev", linkedin: "https://www.linkedin.com/in/aleksey-alekseev" },
-  { name: "Мария Мариева", specialization: "Fullstack Developer", git: "https://github.com/maria-marieva", linkedin: "https://www.linkedin.com/in/maria-marieva" },
-  { name: "Николай Николаев", specialization: "Fullstack Developer", git: "https://github.com/nikolay-nikolayev", linkedin: "https://www.linkedin.com/in/nikolay-nikolayev" },
-  { name: "Ольга Ольгова", specialization: "Fullstack Developer", git: "https://github.com/olga-olgova", linkedin: "https://www.linkedin.com/in/olga-olgova" },
+  { name: "Liubov Korchashkina", specialization: "Fullstack Developer", git: "", linkedin: "" },
+  { name: "Vladyslav Velykyy", specialization: "Fullstack Developer", git: "", linkedin: "" },
+  { name: "Natalia Boiko", specialization: "Fullstack Developer", git: "", linkedin: "" },
+  { name: "Kateryna Ushenko", specialization: "Fullstack Developer", git: "", linkedin: "" },
+  { name: "Koryun Azaryan", specialization: "Fullstack Developer", git: "", linkedin: "" },
 ];
 
 const testers = [
-   { name: "Анастасия Анастасиева", specialization: "QA Manual and Automation Tester", git: "", linkedin: "" },
-  { name: "Дмитрий Дмитриев", specialization: "QA Manual and Automation Tester", git: "", linkedin: "" }
+   { name: "Serhii Shvets", specialization: "QA Manual and Automation Tester", git: "", linkedin: "" },
+  { name: "Anna Schaedlich", specialization: "QA Manual and Automation Tester", git: "", linkedin: "" }
   ];
 
 const AboutUs: React.FC = () => {
