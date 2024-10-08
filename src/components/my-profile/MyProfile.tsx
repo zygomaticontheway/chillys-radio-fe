580597dca292d6d1228cf6d48362049d67419bcd
zygomaticontheway <mrzygomatic@gmail.com>
2024-10-08 15:29:06 +0200

    password changed form style fix

--- src/components/my-profile/MyProfile.tsx
+++ src/components/my-profile/MyProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'; //andern
+import React, { useEffect, useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../../redux/hooks';
 import Loader from '../loader/Loader';
 import MyButton from '../myButton/MyButton';
@@ -13,12 +13,12 @@ import PasswordChangeForm from '../user-change-password/ChangePasswordForm';
 const MyProfile: React.FC = () => {
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
-    const [isFormVisible, setFormVisible] = useState(false);// andern
+    const [isFormVisible, setFormVisible] = useState(false);
     const { user, isLoading } = useAppSelector(state => state.user);
 
-    const handleChangePasswordClick = () => {      //
-        setFormVisible(!isFormVisible); // andern
-    };                                             //
+    const handleChangePasswordClick = () => {
+        setFormVisible(!isFormVisible);
+    };
 
     useEffect(() => {
         const token = localStorage.getItem('user-token');
@@ -59,12 +59,12 @@ const MyProfile: React.FC = () => {
             </div>
 
             <div className={styles.actions}>
-                <MyButton name="Change Password" onClick={handleChangePasswordClick} className={styles.button} />  
+                <MyButton name="Change Password" onClick={handleChangePasswordClick} className={styles.button} />
                 <MyButton name="Logout" onClick={handleLogout} className={`${styles.button} ${styles.logoutButton}`} />
             </div>
             {isFormVisible && (
-                <div className='password-form-container'> 
-                  <PasswordChangeForm />
+                <div className='password-form-container'>
+                    <PasswordChangeForm />
                 </div>
             )}
 
